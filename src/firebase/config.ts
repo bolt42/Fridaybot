f96e4595eddae88ec02f1ddd82fa9b1c0361526e
BOLT4L <Eyobwasihun401@gmail.com>
2025-08-29 10:36:10 +0300

    ll

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
@@ -19,7 +18,6 @@ if (typeof window !== 'undefined') {
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
 export const rtdb = getDatabase(app);
 export const auth = getAuth(app);
 

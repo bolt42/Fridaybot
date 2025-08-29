dbc4b1fdcf4e8e106f20694d324924ce8f0b8b49
BOLT4L <Eyobwasihun401@gmail.com>
2025-08-29 10:03:33 +0300

    SS

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBRxd0gLACHrmQ5Jk0RkaY-tjYz9LhKx7g",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "jackpot-fd988.firebaseapp.com",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "jackpot-fd988",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "jackpot-fd988.firebasestorage.app",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "jackpot-fd988.appspot.com",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "859370808626",
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:859370808626:web:851e1452e9e0ec3b66232b"
 };

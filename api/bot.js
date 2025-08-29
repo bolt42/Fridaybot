fb37fe8127eeaa970aebcf3373805584d8a9e3ba
BOLT4L <Eyobwasihun401@gmail.com>
2025-08-29 19:43:40 +0300

    cc

--- api/bot.js
+++ api/bot.js
@@ -421,7 +421,13 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+
   if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  if (req.method === "HEAD") {
     res.status(200).end();
     return;
   }

6a00d2cbac8b5d1c32e2fd015e04fc3ee6d6e256
BOLT4L <Eyobwasihun401@gmail.com>
2025-09-02 23:00:09 +0300

    as

--- api/start-game.js
+++ api/start-game.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
 
       // update room status
       room.gameStatus = "playing";
-      room.activeGameId = gameId;
+      room.gameId = gameId;
 
       return room;
     });
